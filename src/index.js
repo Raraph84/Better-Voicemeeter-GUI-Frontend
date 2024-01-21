528f042e1a8094d7b6022cae295e380871a1f2cf
Raraph84 <contact@raraph.fr>
2024-01-21 11:43:56 +0100

    Split outputs/inputs into components ; Add label edit to inputs/outputs

--- src/index.js
+++ src/index.js
@@ -9,25 +9,12 @@ class App extends Component {
 
         super(props);
 
-        this.initialized = false;
         this.leveling = false;
 
-        this.firstInputGainRef = createRef();
-        this.firstOutputGainRef = createRef();
-
-        this.state = {
-            inputs: [],
-            outputs: [],
-            inputsGainWidth: 0,
-            inputsGainHeight: 0,
-            outputsGainWidth: 0,
-            outputsGainHeight: 0
-        };
+        this.state = { inputs: [], outputs: [] };
     }
 
     componentDidMount() {
-
-        window.addEventListener("resize", this.updateSliderSizes.bind(this));
 
         window.api.on("config", this.onApiConfig.bind(this));
         window.api.on("levels", this.onApiLevels.bind(this));
@@ -36,15 +23,12 @@ class App extends Component {
 
     onApiConfig(event) {
 
-        this.setState({ inputs: event.inputs, outputs: event.outputs }, () => {
-            this.initialized = true;
-            setTimeout(() => this.updateSliderSizes(), 10);
-        });
+        this.setState({ inputs: event.inputs, outputs: event.outputs });
     }
 
     onApiLevels(event) {
 
-        if (!this.initialized || this.leveling) return;
+        if (this.leveling) return;
 
         this.leveling = true;
 
@@ -71,15 +55,45 @@ class App extends Component {
         }, () => this.leveling = false);
     }
 
+    render() {
+
+        return <>
+
+            <div className="inputs">
+                {this.state.inputs.map((input) => <Input key={input.id} input={input} />)}
+            </div>
+
+            <div className="outputs">
+                {this.state.outputs.map((output) => <Output key={output.id} output={output} />)}
+            </div>
+
+        </>;
+    }
+}
+
+class Input extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.gainRef = createRef();
+
+        this.state = { gainWidth: 0, gainHeight: 0, editingLabel: false };
+    }
+
+    componentDidMount() {
+
+        setTimeout(() => this.updateSliderSizes(), 100);
+
+        window.addEventListener("resize", this.updateSliderSizes.bind(this));
+    }
+
     updateSliderSizes() {
 
-        if (!this.initialized) return;
-
         this.setState({
-            inputsGainWidth: this.firstInputGainRef.current.clientHeight,
-            inputsGainHeight: this.firstInputGainRef.current.clientWidth,
-            outputsGainWidth: this.firstOutputGainRef.current.clientHeight,
-            outputsGainHeight: this.firstOutputGainRef.current.clientWidth
+            gainWidth: this.gainRef.current.clientHeight,
+            gainHeight: this.gainRef.current.clientWidth
         });
     }
 
@@ -91,67 +105,95 @@ class App extends Component {
             window.api.send("config", { inputs: [{ id, gain }] })
         }
 
+        return <div className="input">
+            {!this.state.editingLabel
+                ? <div className="label" onClick={() => this.setState({ editingLabel: true })}>{this.props.input.label || "Unnamed"}</div>
+                : <input className="label" defaultValue={this.props.input.label} autoFocus
+                    onKeyDown={(event) => { if (event.key === "Enter") { window.api.send("config", { inputs: [{ id: this.props.input.id, label: event.target.value }] }); this.setState({ editingLabel: false }); } }} />}
+            <div className="column">
+                <div className={this.props.input.mute ? "level muted" : "level"}>
+                    <div><div style={{ height: (this.props.input.leftLevel || 0) + "%" }} /></div>
+                    <div><div style={{ height: (this.props.input.rightLevel || 0) + "%" }} /></div>
+                </div>
+                <div className="gain" ref={this.gainRef}>
+                    <input type="range" min={-60} max={12} step={0.1} value={this.props.input.gain}
+                        style={{ width: this.state.gainWidth + "px", height: this.state.gainHeight + "px" }}
+                        onInput={(event) => setInputGain(this.props.input.id, parseFloat(event.target.value))}
+                        onDoubleClick={() => setInputGain(this.props.input.id, 0)}
+                        onWheel={(event) => setInputGain(this.props.input.id, this.props.input.gain + (event.deltaY < 0 ? 3 : -3))} />
+                </div>
+                <div className="buttons">
+                    <div className="gain-value">{Math.round(this.props.input.gain * 10) / 10}dB</div>
+                    {this.props.input.outputs.map((output) => <button key={output.id} className={output.enabled ? "active" : ""}
+                        onClick={() => window.api.send("config", { inputs: [{ id: this.props.input.id, outputs: [{ id: output.id, enabled: !output.enabled }] }] })}>
+                        {output.name}
+                    </button>)}
+                    <button className={this.props.input.mute ? "mute active" : "mute"}
+                        onClick={() => window.api.send("config", { inputs: [{ id: this.props.input.id, mute: !this.props.input.mute }] })}>Mute</button>
+                </div>
+            </div>
+        </div>;
+    }
+}
+
+class Output extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.gainRef = createRef();
+
+        this.state = { gainWidth: 0, gainHeight: 0, editingLabel: false };
+    }
+
+    componentDidMount() {
+
+        setTimeout(() => this.updateSliderSizes(), 100);
+
+        window.addEventListener("resize", this.updateSliderSizes.bind(this));
+    }
+
+    updateSliderSizes() {
+
+        this.setState({
+            gainWidth: this.gainRef.current.clientHeight,
+            gainHeight: this.gainRef.current.clientWidth
+        });
+    }
+
+    render() {
+
         const setOutputGain = (id, gain) => {
             if (gain < -60) gain = -60;
             if (gain > 12) gain = 12;
             window.api.send("config", { outputs: [{ id, gain }] })
         }
 
-        return <>
-
-            <div className="inputs">
-                {this.state.inputs.map((input) => <div key={input.id} className="input">
-                    <div className="label">{input.label}</div>
-                    <div className="column">
-                        <div className={input.mute ? "level muted" : "level"}>
-                            <div><div style={{ height: (input.leftLevel || 0) + "%" }} /></div>
-                            <div><div style={{ height: (input.rightLevel || 0) + "%" }} /></div>
-                        </div>
-                        <div className="gain" ref={input.id === 0 ? this.firstInputGainRef : undefined}>
-                            <input type="range" min={-60} max={12} step={0.1} value={input.gain}
-                                style={{ width: this.state.inputsGainWidth + "px", height: this.state.inputsGainHeight + "px" }}
-                                onInput={(event) => setInputGain(input.id, parseFloat(event.target.value))}
-                                onDoubleClick={() => setInputGain(input.id, 0)}
-                                onWheel={(event) => setInputGain(input.id, input.gain + (event.deltaY < 0 ? 3 : -3))} />
-                        </div>
-                        <div className="buttons">
-                            <div className="gain-value">{Math.round(input.gain * 10) / 10}dB</div>
-                            {input.outputs.map((output) => <button key={output.id} className={output.enabled ? "active" : ""}
-                                onClick={() => window.api.send("config", { inputs: [{ id: input.id, outputs: [{ id: output.id, enabled: !output.enabled }] }] })}>
-                                {output.name}
-                            </button>)}
-                            <button className={input.mute ? "mute active" : "mute"}
-                                onClick={() => window.api.send("config", { inputs: [{ id: input.id, mute: !input.mute }] })}>Mute</button>
-                        </div>
-                    </div>
-                </div>)}
+        return <div className="output">
+            {!this.state.editingLabel
+                ? <div className="label" onClick={() => this.setState({ editingLabel: true })}>{this.props.output.label || "Unnamed"}</div>
+                : <input className="label" defaultValue={this.props.output.label} autoFocus
+                    onKeyDown={(event) => { if (event.key === "Enter") { window.api.send("config", { outputs: [{ id: this.props.output.id, label: event.target.value }] }); this.setState({ editingLabel: false }); } }} />}
+            <div className="column">
+                <div className={this.props.output.mute ? "level muted" : "level"}>
+                    <div><div style={{ height: (this.props.output.leftLevel || 0) + "%" }} /></div>
+                    <div><div style={{ height: (this.props.output.rightLevel || 0) + "%" }} /></div>
+                </div>
+                <div className="gain" ref={this.gainRef}>
+                    <input type="range" min={-60} max={12} step={0.1} value={this.props.output.gain}
+                        style={{ width: this.state.gainWidth + "px", height: this.state.gainHeight + "px" }}
+                        onInput={(event) => setOutputGain(this.props.output.id, parseFloat(event.target.value))}
+                        onDoubleClick={() => setOutputGain(this.props.output.id, 0)}
+                        onWheel={(event) => setOutputGain(this.props.output.id, this.props.output.gain + (event.deltaY < 0 ? 3 : -3))} />
+                </div>
+                <div className="buttons">
+                    <div className="gain-value">{Math.round(this.props.output.gain * 10) / 10}dB</div>
+                    <button className={this.props.output.mute ? "mute active" : "mute"}
+                        onClick={() => window.api.send("config", { outputs: [{ id: this.props.output.id, mute: !this.props.output.mute }] })}>Mute</button>
+                </div>
             </div>
-
-            <div className="outputs">
-                {this.state.outputs.map((output) => <div key={output.id} className="output">
-                    <div className="label">{output.label}</div>
-                    <div className="column">
-                        <div className={output.mute ? "level muted" : "level"}>
-                            <div><div style={{ height: (output.leftLevel || 0) + "%" }} /></div>
-                            <div><div style={{ height: (output.rightLevel || 0) + "%" }} /></div>
-                        </div>
-                        <div className="gain" ref={output.id === 0 ? this.firstOutputGainRef : undefined}>
-                            <input type="range" min={-60} max={12} step={0.1} value={output.gain}
-                                style={{ width: this.state.outputsGainWidth + "px", height: this.state.outputsGainHeight + "px" }}
-                                onInput={(event) => setOutputGain(output.id, parseFloat(event.target.value))}
-                                onDoubleClick={() => setOutputGain(output.id, 0)}
-                                onWheel={(event) => setOutputGain(output.id, output.gain + (event.deltaY < 0 ? 3 : -3))} />
-                        </div>
-                        <div className="buttons">
-                            <div className="gain-value">{Math.round(output.gain * 10) / 10}dB</div>
-                            <button className={output.mute ? "mute active" : "mute"}
-                                onClick={() => window.api.send("config", { outputs: [{ id: output.id, mute: !output.mute }] })}>Mute</button>
-                        </div>
-                    </div>
-                </div>)}
-            </div>
-
-        </>;
+        </div>;
     }
 }
 
